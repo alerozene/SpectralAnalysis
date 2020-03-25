b2399f3f58ef860b12544e9280d35939938ef8bb
alerozene <alejandrorv68@gmail.com>
2020-03-25 17:40:03 +0100

    Update assignment_three.m

--- assignment_three.m
+++ assignment_three.m
@@ -8,39 +8,52 @@ A.bc = 'periodic';
 x = chebfun('x', d);
 u0 = 4*exp(-x^2);
 A.op = @(u) diff(u,2)+u0*u;
-[vv,D] = eigs(A,50);
-max(D(:))
+[eigfuns,eigvals] = eigs(A,50);
+max(eigvals(:))
 %% Task 2. Verify recovery
-u_0z = D(1,1)*vv{1}^2;
-for ii=2:length(D)
-    u_0z = D(ii,ii)*vv{ii}^2;
+u_0z = eigvals(1,1)*eigfuns{1}^2;
+for ii=2:length(eigvals)
+    u_0z = eigvals(ii,ii)*eigfuns{ii}^2;
 end
 %% Task 3
-lambda = diag(D);
-diff_eigenfunc = diff(vv);
-innervec = sum(vv{1}, -2*pi, 2*pi);
-for ii=2:length(D)
-    innervec = sum(vv{ii}, -2*pi, 2*pi);
+lambda = diag(eigvals);
+diff_eigenfunc = diff(eigfuns);
+innervec = sum(eigfuns{1}, -2*pi, 2*pi);
+for ii=2:length(eigvals)
+    innervec = sum(eigfuns{ii}, -2*pi, 2*pi);
 end 
 %% Task 4
+fv0 = reshape([diff_eigenfunc; eigfuns], [], 1);
+
+
 %%[TOUT,YOUT] = ode113(ODEFUN,TSPAN,Y0);
 %% Task 5 KdV numerically (as in chebfun.org)
 tmax = 0.1;
 S = spinop(d,[0 tmax]);
 S.lin = @(u) - diff(u,3);
-S.nonlin = @(u) -6*diff(u.^2); % spin cannot parse "u.*diff(u)"
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+S.nonlin = @(u) -3*diff(u.^2); % spin cannot parse "u.*diff(u)"
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 S.init = u0;
 N = 800;   % numer of grid points
 dt = 5e-6; % time-step
 u_t01 = spin(S,N,dt,'plot','off');
 %% Task 6 
-
 A_t01 = chebop(d); 
 A_t01.bc = 'periodic';
 A_t01.op = @(u) diff(u,2)+u_t01*u;
-[vv,D] = eigs(A_t01,50);
-max(D(:))
+[eigfunsComp,eigvalsCom] = eigs(A_t01,50);
+max(eigvalsCom(:))
 
+Almat = zeros(N*2);
+for ii=1:size(eigvals,1)
+    Almat(1,2) = -4*eigvals(1,1)^2;
+    Almat(2,1) = -4*eigvals(1,1);
+end
+
+expm()
+
+%% Task 7
 %% Plots
 figure;
 
